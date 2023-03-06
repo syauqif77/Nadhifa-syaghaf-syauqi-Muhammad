e7730d7807ba5f70d00b94fe75a1f458c9867d82
syauqif77 <syagfm777@gmail.com>
2023-03-06 12:07:42 +0700

    menambahkan latihan1.cpp

---
+++ latihan1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()(
+    // Begin
+    // Numeric Luas,p,l
+    double Luas, p, l;
+    // display 'Masukan Panjangnya = '
+    cout << "Masukan Panjangnya =";
+    // Accept p
+    cin >> p;
+    / display 'Masukan Lebarnya'
+    cout ,, "Masukan Lebarnya = ";
+    // Accept 1
+    cin >> 1;
+    // Compute Luas = p*l
+    Luas = p*l
+    // display "Luasnya = "+Luas
+    cout << "Luasnya =" << Luas;
+    // End
+
+    x
